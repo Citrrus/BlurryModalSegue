73e9904623bf212b259e8cd45088b3c263137fa2
Matt Hupman <mhupman@gmail.com>
2013-11-21 22:13:00 -0500

    Clean up demo project, add a little more pizazz to the demo project

--- BlurryModal/ViewController.m
+++ BlurryModal/ViewController.m
@@ -8,26 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
-
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (IBAction)unwindToThisViewController:(UIStoryboardSegue *)unwindSegue
-{
-}
+- (IBAction)unwindToThisViewController:(UIStoryboardSegue *)unwindSegue {}
 
 @end
